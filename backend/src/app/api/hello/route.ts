00e496a29e753fc5ccfae7027b37bc69d1714a29
Chris <christian.hadianto@gmail.com>
2025-06-07 17:19:41 +0700

    fixing cors

--- backend/src/app/api/hello/route.ts
+++ backend/src/app/api/hello/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-
-  const productionOrigin = process.env.FRONTEND_URL ?? "";
-  return NextResponse.json({ message: "url: " + productionOrigin })
+  
+  return NextResponse.json({ message: "" })
 }
