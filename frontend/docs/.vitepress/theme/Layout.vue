570861db2d03c19c874df67ec71223099f684274
Chris <christian.hadianto@gmail.com>
2025-06-11 11:04:34 +0700

    fix library path

--- frontend/docs/.vitepress/theme/Layout.vue
+++ frontend/docs/.vitepress/theme/Layout.vue
@@ -12,11 +12,11 @@
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 import { computed } from 'vue'
-import SpotifyBubble from '../../components/SpotifyBubble.vue' // Adjust path as needed
-import NaitBubble from '../../components/Assistant/NaitBubble.vue'
+import SpotifyBubble from '../../components/SpotifyBubble.vue' 
+import NaitBubble from '../../components/assistant/NaitBubble.vue'
+
 const { Layout } = DefaultTheme
 const { page } = useData()
-
 const pagesToHideBubble = ['index.md', 'time-machine.md'];
 const isBubbleHiddenPage = computed(() => {
   return pagesToHideBubble.includes(page.value.relativePath);
