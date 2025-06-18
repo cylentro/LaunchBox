68f45fd197bf57e5d4df8ea3d6c1ee77234921e6
Chris <christian.hadianto@gmail.com>
2025-06-18 08:46:15 +0700

    fix maximize windows in mobile view

--- frontend/docs/components/assistant/NaitBubble.vue
+++ frontend/docs/components/assistant/NaitBubble.vue
@@ -26,7 +26,7 @@
                             Clear Session
                         </button>
                         <button @click="toggleMaximize"
-                            class="p-1 text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 rounded-full hover:bg-slate-100 dark:hover:bg-neutral-700 flex items-center justify-center mr-1.5"
+                            class="p-1 text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-200 rounded-full hover:bg-slate-100 dark:hover:bg-neutral-700 hidden sm:flex items-center justify-center mr-1.5"
                             :title="isMaximized ? 'Restore Down' : 'Maximize'"
                             :aria-label="isMaximized ? 'Restore chat popup to normal size' : 'Maximize chat popup'">
                             <!-- Maximize Icon -->
